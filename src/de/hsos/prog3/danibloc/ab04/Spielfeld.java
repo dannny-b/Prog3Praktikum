933b837b2606138d6958276544cad9d792baa6b5
danibloc <daniel.block@hs-osnabrueck.de>
2022-11-16 22:03:14 +0100

    CircularBuffer vervollständigt

--- src/de/hsos/prog3/danibloc/ab04/Spielfeld.java
+++ src/de/hsos/prog3/danibloc/ab04/Spielfeld.java
@@ -1,4 +1,20 @@
 package de.hsos.prog3.danibloc.ab04;
 
-public class Spielfeld {
+
+
+public class Spielfeld extends Interaktionsbrett {
+
+    int breite, hoehe;
+    private final int margin = 15;
+    Rechteck spielflaeche;
+    public Spielfeld(int breite, int hoehe){
+        spielflaeche = new Rechteck(margin, margin, breite, hoehe);
+        this.breite = breite; this.hoehe = hoehe;
+    }
+
+    public void darstellen(Interaktionsbrett ib){
+        ib.abwischen();
+        ib.neuesRechteck(spielflaeche, "spielfeld", margin, margin, breite, hoehe);
+        ib.neueLinie(spielflaeche.mitteInX(), spielflaeche.mitteInY(), spielflaeche.mitteInX()+spielflaeche.getHoehe(), spielflaeche.mitteInY()+ spielflaeche.getHoehe());
+    }
 }
